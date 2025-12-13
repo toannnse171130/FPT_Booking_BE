0a4b7f4c4068d16565e68e637eeae3a287452b19
toannnse171130 <toannnse171130@fpt.edu.vn>
2025-12-13 21:49:21 +0700

    Create UpdateConditionRequest.cs

---
+++ DTOs/UpdateConditionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FPT_Booking_BE.DTOs
+{
+    public class UpdateConditionRequest
+    {
+        public int Id { get; set; }
+        public string Condition { get; set; } = "Good";
+        public int? Quantity { get; set; }
+    }
+}
